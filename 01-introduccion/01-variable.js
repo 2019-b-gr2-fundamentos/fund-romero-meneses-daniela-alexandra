c594dd502dc5532ea0cb25c690e9f4ab9698f9f4
DanielaRomero <daniela.romero@epn.edu.ec>
2019-10-17 18:03:18 -0500

    Clase 17/10/2019 Romero Daniela

---
+++ 01-introduccion/01-variable.js
@@ -0,0 +1,34 @@
+/* 
+Nombre: Daniela Romero
+Fecha de Actualización: 17-10-2019
+Descripción: Ejemplos de variable primitivas en JS
+*/
+// Comentario
+/* <- abro bloque comentario
+cierro bloque comentario -> */
+
+//Primitivas
+/*
+int         -> Número entero       Ej: -5,0,1,2,3
+char        -> Caracter            Ej: a, b, c
+float       -> Números decimales   Ej: 3.1415, 524.369
+double      -> N. Decimales ++     Ej: 468786453614...
+string      -> 
+null        -> VACIO               Ej: null
+boolean     -> Verdadero/Falso     Ej: True or False
+*/
+// var edad = 19; // -> ya no se usa NO USAR
+let edad = 19; // Mutable -> Cambiar
+// const edad = 19; // Inmutable -> No Cambiar
+const IVA = 12;
+// IVA = 14; // NO SE PUEDE REASIGNAR VALORES A UNA VARIABLE CONSTANTE (INMUTABLE)
+let sueldo = 0;
+// edad = 30 -> python
+// var edad: Integer = 30; -> kotlin Mutable
+// val edad: Integer = 30; -> kotlin Inmutable
+
+/* 
+Siempre que se pueda se debe tener variables INMUTABLES
+*/
+// cons IVA = 12;
+// let IVA = 12;
---
+++ 01-introduccion/02-variables-ts.ts
@@ -0,0 +1,7 @@
+var edad = 19;
+//let edad = 19; 
+//const edad = 19; 
+const IVA = 12;
+let sueldo = 0;
+//const IVA = 12;
+//let IVA = 12;
