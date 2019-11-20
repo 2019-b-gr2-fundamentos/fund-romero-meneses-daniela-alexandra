ed28c54ef45610767411c98e9b5e4506967de2a4
Daniela Romero <daniela.romero@epn.edu.ec>
2019-11-20 11:03:47 -0500

    floron/alcance

--- 02-iterables/ejercicio-del-floron .ts
+++ 02-iterables/ejercicio-del-floron .ts
@@ -6,9 +6,7 @@ Ej: [0, 0, 0, 0, 0] NO ES VALIDO
 Ej: [0, 0, 0, 0, 1] ES VALIDO
 */
 const floron = [];
-floron.length = 5;
-
-const jugadores = 5;
+const jugadores = 2;
 
 // Desarrollamos el juego del floron donde 1 significa que la persona tiene el floron y el 0 que no tiene
 
@@ -16,7 +14,10 @@ for(let elementos = 0; elementos < jugadores ; elementos ++ ){
     const quienTieneElFloron = (Math.floor(Math.random()*2));
     floron.push(quienTieneElFloron);
     console.log(quienTieneElFloron);
-    if(floron == [0, 0, 0, 0, 0]){
-        console.log("[1, 0, 0, 0, 1]")
+    for(let indice = 0; indice < jugadores; indice ++){
+        let existeFloron = false;
+        if(floron[indice] == 1){
+            existeFloron = true;
+        }
     }
 }
