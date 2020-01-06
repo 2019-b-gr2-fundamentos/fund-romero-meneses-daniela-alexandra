dd7534f42e1128d73e4bfbd15171d2ab251032f3
DiegoA <diego.pullotasig@epn.edu.ec>
2020-01-06 09:12:32 -0500

    06/enero

---
+++ 05-proyecto/index.js
@@ -0,0 +1,7 @@
+//NODE MODULES
+// const sumaLibreria = require('04-librerias/suma');
+// Importar todo el contenido con el nombre 'sumaLiberia' 
+//del paquete '04-libreria/suma'
+var sumaLibreria = require('04-librerias/suma');
+var respuesta = sumaLibreria(1, 2);
+console.log('la respuesta es', respuesta);
