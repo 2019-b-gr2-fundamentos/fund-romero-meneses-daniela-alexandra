11b1963f232593fc15605176c936420517968c64
Daniela Romero <daniela.romero@epn.edu.ec>
2020-01-30 17:54:56 -0500

    30/01

---
+++ 08-operadores/04-map.ts
@@ -0,0 +1,11 @@
+export function map(
+    arreglo: any[],
+    funcion: (
+        valorActual: any,
+        indice?: number,
+        arreglo?: any[]) => boolean): any[]{
+            const arregloNuevo = [];
+
+            
+            return arregloNuevo
+    }
