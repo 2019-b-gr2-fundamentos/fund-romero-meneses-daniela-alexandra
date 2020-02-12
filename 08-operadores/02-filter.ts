dba21ba0058fb27a0ca7a2912029eac5ac66d72d
DanielaRomero <55814839+DanielaRomero@users.noreply.github.com>
2020-02-12 14:36:50 -0500

    Deber

--- 08-operadores/02-filter.ts
+++ 08-operadores/02-filter.ts
@@ -1,22 +1,3 @@
-export function calcular(funcion, numUno: number, numDos: number){
-    return funcion(numUno, numDos)
-}
-export function sumar(numUno: number, numDos: number): number{
-    return numUno + numDos
-};
-export function restar(numUno: number, numDos: number): number{
-    return numUno - numDos
-}
-calcular(sumar, 1,2);
-calcular(
-    function(numUno, numDos, valorInicial){
-        return numUno - numDos
-    },
-    5,
-    3
-);
-
-
 export function filter(arreglo: any[],
     funcion: (
         valorActual: any, 
